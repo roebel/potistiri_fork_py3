25c1d6c7ba2c8faad9896e54d8a71e24399fe049
Alexandros <irregulator@riseup.net>
2016-03-02 22:20:18 +0200

    Adds support for providers with LDAP authentication

--- potistiri.py
+++ potistiri.py
@@ -55,12 +55,21 @@ def pass_post(up_pass, expire, one_time, file_key):
     return post_params
 
 
-def ldap_post():
+def ldap_post(ldap_user, ldap_pass, expire, one_time, file_key):
     '''
     This function constructs POST parameters
     for coquelicot instances with LDAP authentication.
     '''
-    pass
+    post_params = [
+        ('ldap_user', str(ldap_user)),
+        ('ldap_password', str(ldap_pass)),
+        ('any_number', 'true' if not one_time else ''),
+        ('expire', str(expire)),
+        ('one_time', 'true' if one_time else ''),
+        ('file_key', str(file_key)),
+        ]
+
+    return post_params
 
 
 def read_conf(arg):
@@ -93,15 +102,26 @@ def offer_init():
             except IOError:
                 exit(1)
             c = cfgparse.RawConfigParser()
-            service_mes = 'Type the name of the service: '
+            service_mes = 'Type a short name of the service: '
             url_mes = 'Type the https:// address of the file server: '
+            authtype_mes = 'Authentication offered by this provider, ' + \
+                           'type "simple" or "ldap": '
             paswd_mes = 'Type the upload pass used for this server: '
+            ldap_user_mes = 'Type the ldap user: '
+
             section = raw_input(service_mes)
             url = raw_input(url_mes)
-            paswd = raw_input(paswd_mes)
             c.add_section(section)
             c.set(section, 'server', url)
-            c.set(section, 'pass', paswd)
+
+            authtype = raw_input(authtype_mes)
+            c.set(section, 'type', authtype)
+            if authtype == 'simple':
+                paswd = raw_input(paswd_mes)
+                c.set(section, 'pass', paswd)
+            elif authtype == 'ldap':
+                ldap_user = raw_input(ldap_user_mes)
+                c.set(section, 'user', ldap_user)
             with open(conf_dir + 'servers.conf', 'wb') as f:
                 c.write(f)
         else:
@@ -113,7 +133,6 @@ def offer_init():
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--server', '-s', dest='server')
-    parser.add_argument('--upload-password', '-p', dest='up_pass')
     parser.add_argument('--expire', '-e', dest='expire', default='60')
     parser.add_argument('--one-time', '-o', dest='one_time',
                         action='store_true')
@@ -122,6 +141,9 @@ def main():
     ga = parser.add_mutually_exclusive_group(required=True)
     ga.add_argument('--file', '-f', dest='filepath')
     ga.add_argument('--setconf', dest='setconf', action='store_true')
+    gb = parser.add_mutually_exclusive_group()
+    gb.add_argument('--ldap-user', '-u', dest='ldapuser')
+    gb.add_argument('--upload-password', '-p', dest='up_pass')
     args = parser.parse_args()
 
     if args.setconf:
@@ -132,11 +154,27 @@ def main():
             exit('{} not found or not readable.'.format(fpath))
         if args.server is None:
             args.server = read_conf('server')
-        if args.up_pass is None:
+            if not args.server:
+                print('Missing server argument.')
+                exit('Try --setconf to store it in a config file.')
+            server_type = read_conf('type')
+
+        if args.up_pass is None and server_type == 'simple':
             args.up_pass = read_conf('pass')
-        if not (args.server and args.up_pass):
-            print('Missing server and/or upload-password arguments')
+        if args.ldapuser is None and server_type == 'ldap':
+            args.ldapuser = read_conf('user')
+            ldap_pass = read_conf('pass')
+        if not (args.up_pass or args.ldapuser):
+            print('Missing connection arguments')
             exit('Try --setconf to store them in a config file.')
+        if args.ldapuser and server_type == 'simple':
+            exit('Cannot use ldap authentication with this type of provider.')
+        if args.up_pass and server_type == 'ldap':
+            exit('Cannot use simple pass authentication with ldap provider.')
+        if args.ldapuser and not ldap_pass:
+            m = 'Type ldap password: '
+            ldap_pass = getpass.getpass(m)
+
         if args.file_key:
             m = 'Type passphrase to lock the uploaded file. ' + \
                 'Or just hit enter to create one for you: '
@@ -144,13 +182,19 @@ def main():
             if not file_key:
                 file_key = ''.join(choice(lowercase+digits) for _ in range(25))
 
-        print(aneva(
-            args.server,
-            pass_post(args.up_pass,
-                      args.expire,
-                      args.one_time,
-                      file_key if args.file_key else ''),
-            fpath))
+        if args.up_pass:
+            post_params = pass_post(args.up_pass,
+                                    args.expire,
+                                    args.one_time,
+                                    file_key if args.file_key else '')
+        else:
+            post_params = ldap_post(args.ldapuser,
+                                    ldap_pass,
+                                    args.expire,
+                                    args.one_time,
+                                    file_key if args.file_key else '')
+
+        print(aneva(args.server, post_params, fpath))
         if args.file_key:
             print('Download pass: {}'.format(file_key))
 
